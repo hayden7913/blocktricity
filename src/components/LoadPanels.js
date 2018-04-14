a888e19340c3af85b0e73ba385e49b48729a4159
Alexis Fasquel <alexis@pch.net>
2018-04-14 16:44:14 -0700

    Colors and data

--- src/components/LoadPanels.js
+++ src/components/LoadPanels.js
@@ -24,72 +24,101 @@ const user2 = {
 }
 
 
-const data = {
+const RepDataPerso = {
 	labels: [
-		'Red',
-		'Blue',
-		'Yellow'
+		'Wind',
+		'Solar',
+		'Grid'
 	],
 	datasets: [{
-		data: [300, 50, 100],
+        data: [63, 10, 26],
 		backgroundColor: [
-		'#FF6384',
+        '#FF6384',
+        '#FFCE56',
 		'#36A2EB',
-		'#FFCE56'
 		],
 		hoverBackgroundColor: [
-		'#FF6384',
+        '#FF6384',
+        '#FFCE56',
 		'#36A2EB',
-		'#FFCE56'
+		]
+	}]
+};
+
+const RepDataCommunity = {
+	labels: [
+		'Wind',
+		'Solar',
+		'Grid'
+	],
+	datasets: [{
+        data: [63, 21, 15],
+		backgroundColor: [
+        '#FF6384',
+        '#FFCE56',
+		'#36A2EB',
+		],
+		hoverBackgroundColor: [
+        '#FF6384',
+        '#FFCE56',
+		'#36A2EB',
 		]
 	}]
 };
 
 
-const linedata = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+const persoData = {
+//   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  labels: ['19', '20h', '21h', '22h', '23h', '00h','01h', '02h','03h','04h','05h','06h','07h','08h','09h','10h','11h','12h','13h','14h', '15h', '16h', '17h', '18h'],
   datasets: [
     {
-      label: '',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
+        label: 'Personal load',
+        fill: false,
+        lineTension: 0.1,
+        borderColor: '#FF6384',
+        pointBorderColor: '#FF6384',
+        pointBackgroundColor: '#fff',
+        data: [1465.5,1349.7,1295.7,1277.7,1274.1,1587.6,1312.2,1122,1198.2,1510.5,1471.5,1438.8,1437.6,1418.4,1370.1,1328.1,1352.7,1453.2,1591.2,1693.5,1764,1849.2,1732.5,1629.3]
+    },
+    {
+        label: 'Average load',
+        fill: false,
+        lineTension: 0.1,
+        borderColor: '#36A2EB',
+        pointBorderColor: '#36A2EB',
+        pointBackgroundColor: '#fff',
+        data: [1573.8,1453.5,1387.2,1351.2,1337.4,1341.9,1352.1,1446.6,1531.2,1568.7,1571.4,1575.3,1588.5,1589.4,1539.3,1498.2,1512.6,1594.2,1710,1803.3,1865.4,1947,1846.2,1742.4
+        ]
+      },
+
   ]
 };
 
-const styles = {
-    card: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      marginBottom: 16,
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-};
+const communityData = {
+    //   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: ['19', '20h', '21h', '22h', '23h', '00h','01h', '02h','03h','04h','05h','06h','07h','08h','09h','10h','11h','12h','13h','14h', '15h', '16h', '17h', '18h'],
+      datasets: [
+        {
+            label: 'Community load',
+            fill: false,
+            lineTension: 0.1,
+            borderColor: '#36A2EB',
+            pointBorderColor: '#36A2EB',
+            pointBackgroundColor: '#fff',
+            data: [1465.5,1349.7,1295.7,1277.7,1274.1,1287.6,1312.2,1422,1498.2,1510.5,1471.5,1438.8,1437.6,1418.4,1370.1,1328.1,1352.7,1453.2,1591.2,1693.5,1764,1849.2,1732.5,1629.3]
+        },
+        {
+            label: 'Average load',
+            fill: false,
+            lineTension: 0.1,
+            borderColor: '#FF6384',
+            pointBorderColor: '#FF6384',
+            pointBackgroundColor: '#fff',
+            data: [1573.8,1453.5,1387.2,1351.2,1337.4,1341.9,1352.1,1446.6,1531.2,1568.7,1571.4,1575.3,1588.5,1589.4,1539.3,1498.2,1512.6,1594.2,1710,1803.3,1865.4,1947,1846.2,1742.4]
+          },
+    
+      ]
+    };
 
 export default class LoadPanels extends React.Component {
   render() {
@@ -103,8 +132,8 @@ export default class LoadPanels extends React.Component {
 
             <CardText>
                 <div style={{display: 'flex', alignItems: 'center', margin: '0 auto', justifyContent: 'center', paddingLeft: '75px'}}>
-                    <Line data={linedata} options={options}/>
-                    <Doughnut data={data} options={options}/>
+                    <Line data={persoData} options={options}/>
+                    <Doughnut data={RepDataPerso} options={options}/>
                 </div>
             </CardText>
         </Card>
@@ -117,8 +146,8 @@ export default class LoadPanels extends React.Component {
     
             <CardText>
                 <div style={{display: 'flex', alignItems: 'center', margin: '0 auto', justifyContent: 'center', paddingLeft: '75px'}}>
-                    <Line data={linedata} options={options}/>
-                    <Doughnut data={data} options={options}/>
+                    <Line data={communityData} options={options}/>
+                    <Doughnut data={RepDataCommunity} options={options}/>
                 </div>
             </CardText>
         </Card>
