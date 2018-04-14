a9bfbe8c76db0ad6a01456361653f2070529e081
hayden7913 <hayden321@gmail.com>
2018-04-14 14:34:57 -0700

    Adds renderCard method to ProfileList

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -4,6 +4,8 @@ import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
+import cards from '../constants/cards';
+console.log(cards);
 import ProfileCard from './ProfileCard';
 import Divider from './Divider';
 
@@ -27,28 +29,29 @@ const cardStyles = {
   }
 }
 
-const GridListExampleSimple = () => (
-  <div style={{ padding: "0 10%" }}>
-    <div style={gridStyles.row} className="row">
-      <div className="col6" style={gridStyles.col}>
-        <ProfileCard
-          title="Energy Conract"
-          image="images/energy-contract.jpg"
-          styles={{ ...cardStyles }}
-          dataTop="76% Capacity Use"
-          dataBottom="1 Overdischarge This Week"
-        />
-      </div>
-      <div className="col6" style={gridStyles.col}>
-      </div>
-    </div>
-    <div style={gridStyles.row} className="row">
-      <div className="col6" style={gridStyles.col}>
-      </div>
-      <div className="col6" style={gridStyles.col}>
-      </div>
-    </div>
-  </div>
-);
+export default class ProfileList extends React.Component {
+  renderCard = (card) => {
+    <ProfileCard
+      {...card}
+    />
+  }
 
-export default GridListExampleSimple;
+  render() {
+    return (
+      <div style={{ padding: "0 10%" }}>
+        <div style={gridStyles.row} className="row">
+          <div className="col6" style={gridStyles.col}>
+            <ProfileCard
+              title="Energy Conract"
+              image="images/energy-contract.jpg"
+              styles={{ ...cardStyles }}
+              dataTop="76% Capacity Use"
+              dataBottom="1 Overdischarge This Week"
+            />
+          </div>
+        </div>
+        <div style={gridStyles.row} className="row"></div>
+      </div>
+    );
+  }
+}
--- src/constants/cards.js
+++ src/constants/cards.js
@@ -4,5 +4,17 @@ export const cards = [
     image:"images/energy-contract.jpg",
     dataTop:"76% Capacity Use",
     dataBottom:"1 Overdischarge This Week",
-  }
+  },
+  {
+    title:"Energy Conract",
+    image:"images/energy-contract.jpg",
+    dataTop:"76% Capacity Use",
+    dataBottom:"1 Overdischarge This Week",
+  },
+  {
+    title:"Energy Conract",
+    image:"images/energy-contract.jpg",
+    dataTop:"76% Capacity Use",
+    dataBottom:"1 Overdischarge This Week",
+  },
 ]
